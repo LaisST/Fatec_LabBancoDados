f2b69d9fc0c403b15a5b79ca02a041258aa123de
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-09-21 10:57:48 -0300

    Create FuncaoMeses.sql

---
+++ Scripts/FuncaoMeses.sql
@@ -0,0 +1,31 @@
+CREATE or ALTER FUNCTION dbo.f_MesExtenso (@mes int)
+RETURNS varchar(20)
+AS
+BEGIN
+ declare @variavel varchar(20) = 'Mês Inválido!'
+ If(@mes = 1)
+    set @variavel = 'Jan'
+ ELSE IF(@mes = 2)
+    set @variavel = 'Fev'
+ ELSE IF(@mes = 3)
+    set @variavel = 'Mar'
+ ELSE IF(@mes = 4)
+    set @variavel = 'Abr'
+ ELSE IF(@mes = 5)
+    set @variavel = 'Mai'
+ ELSE IF(@mes = 6)
+    set @variavel = 'Jun'
+ ELSE IF(@mes = 7)
+    set @variavel = 'Jul'
+ ELSE IF(@mes = 8)
+    set @variavel = 'Ago'
+ ELSE IF(@mes = 9)
+    set @variavel = 'Set'
+ ELSE IF(@mes = 10)
+    set @variavel = 'Out'
+ ELSE IF(@mes = 11)
+    set @variavel = 'Nov'
+ ELSE IF(@mes = 12)
+    set @variavel = 'Dez'
+ return @variavel
+END
